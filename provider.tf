de7ce0c680a2b746040c00249720785e03aa3567
ElevateOps <info@elevateops.am>
2024-03-06 00:21:44 +0100

    Added aws mock resources for infra cost

--- provider.tf
+++ provider.tf
@@ -6,3 +6,12 @@ variable "provider_token" {
 provider "fakewebservices" {
   token = var.provider_token
 }
+
+provider "aws" {
+  region = "us-east-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id = true
+  access_key = "mock_access_key"
+  secret_key = "mock_secret_key"
+}
+
